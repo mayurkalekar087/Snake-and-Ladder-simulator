c366970623f01fd77f10acf7fcfd3b116b4463e7
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-04 23:32:22 +0530

    feat:check for random option

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace Snake_and_Ladder_Program
             Random rand = new Random();
             int checkDice = rand.Next(1, 7);
             Console.WriteLine(checkDice);
+            int snakeladder = rand.Next(1, 4);
+            switch (snakeladder)
+            {
+                case 1:
+                    
+                    Console.WriteLine("player moves ahead ");
+                  
+                    break;
+                case 2:
+                    
+                    Console.WriteLine("Player Moves behind ");
+                    break;
+                case 3:
+                    Console.WriteLine("no play ");
+                    break;
 
+            }
         }
 
 	}
