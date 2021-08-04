7005a03b95c5aec3b28c64ddb0bd1ae36e85123c
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-04 23:11:17 +0530

    feat:UC-1-StartPosition

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Snake_and_Ladder_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game!");
+            int playerposition = 0;
+          
+	    }
+
+	}
+    
+}
